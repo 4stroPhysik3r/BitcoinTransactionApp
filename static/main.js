e83124b29a167d8867608638dff0220e8e7e0315
AlexanderEmbacher <82902135+4stroPhysik3r@users.noreply.github.com>
2024-03-10 21:25:35 +0200

    clean up/ api testing

--- static/main.js
+++ static/main.js
@@ -1,14 +1,16 @@
+const balanceDiv = document.getElementById("balance");
+const transactionsDiv = document.getElementById("transactions");
+const transactionHistoryDiv = document.getElementById("transactionsHistory");
+
 // Fetch balance data from Go server
 fetch("/balance")
    .then((response) => response.json())
    .then((balance) => {
-      const balanceDiv = document.getElementById("balance");
 
       balanceDiv.innerHTML = `
             <h2>Your Balance:</h2>
             <p><strong>BTC Balance:</strong> ${balance.BTC_balance} ₿ / ${numberWithCommas(balance.EUR_balance)} €</p>
             `;
-
    })
    .catch((error) =>
       console.error("Error fetching balance data:", error)
@@ -23,65 +25,60 @@ function numberWithCommas(x) {
 fetch("/transactions")
    .then((response) => response.json())
    .then((data) => {
-      const transactionsDiv = document.getElementById("transactions");
+      transactionsDiv.innerHTML = ""
 
-      if (data.length === 0) {
-         const noHistoryMessage = document.createElement("div");
-         noHistoryMessage.textContent = "No transaction history";
-         transactionsDiv.appendChild(noHistoryMessage);
-      } else {
-         data.forEach((transaction) => {
-            if (!transaction.spent) { // Check if transaction is not spent
-               const transactionDiv = document.createElement("div");
-               transactionDiv.classList.add("transaction");
+      data.forEach((transaction) => {
+         if (!transaction.spent) { // Check if transaction is not spent
+            const transactionDiv = document.createElement("div");
+            transactionDiv.classList.add("transaction");
 
-               transactionDiv.innerHTML = `
+            transactionDiv.innerHTML = `
                <p class="transaction"><strong>Transaction ID:</strong> ${transaction.transaction_id}</p>
                <p class="amount"><strong>Amount:</strong> ${transaction.amount}</p>
                <p class="spent"><strong>Spent:</strong> ${transaction.spent ? "Yes" : "No"}</p>
                <p class="date"><strong>Created At:</strong> ${new Date(transaction.created_at).toLocaleString()}</p>
                `;
 
-               transactionsDiv.appendChild(transactionDiv);
-            }
-         });
-      }
+            transactionsDiv.appendChild(transactionDiv);
+         }
+      });
    })
-   .catch((error) =>
+   .catch((error) => {
+      transactionsDiv.innerHTML = ""
+
+      const noTransactionsMessage = document.createElement("div");
+      noTransactionsMessage.textContent = "No unspent transactions available";
+      transactionsDiv.appendChild(noTransactionsMessage);
       console.error("Error fetching transaction data:", error)
-   );
+   });
 
 // Fetch transaction data from Go server
 fetch("/transactions")
    .then((response) => response.json())
    .then((data) => {
-      const transactionHistoryDiv = document.getElementById("transactionsHistory");
+      transactionHistoryDiv.innerHTML = ""
 
-      // Check if there are any spent transactions
-      const noSpentTransactions = data.every(transaction => !transaction.spent);
+      data.forEach((transaction) => {
+         if (transaction.spent) { // Check if transaction is not spent
+            const transactionHistory = document.createElement("div");
+            transactionHistory.classList.add("transactionHistory");
 
-      if (noSpentTransactions) {
-         const noHistoryMessage = document.createElement("div");
-         noHistoryMessage.textContent = "No transaction history";
-         transactionHistoryDiv.appendChild(noHistoryMessage);
-      } else {
-         data.forEach((transaction) => {
-            if (transaction.spent) { // Check if transaction is not spent
-               const transactionHistory = document.createElement("div");
-               transactionHistory.classList.add("transactionHistory");
-
-               transactionHistory.innerHTML = `
+            transactionHistory.innerHTML = `
                <p class="transaction"><strong>Transaction ID:</strong> ${transaction.transaction_id}</p>
                <p class="amount"><strong>Amount:</strong> ${transaction.amount}</p>
                <p class="spent"><strong>Spent:</strong> ${transaction.spent ? "Yes" : "No"}</p>
                <p class="date"><strong>Created At:</strong> ${new Date(transaction.created_at).toLocaleString()}</p>
                `;
 
-               transactionHistoryDiv.appendChild(transactionHistory);
-            }
-         });
-      }
+            transactionHistoryDiv.appendChild(transactionHistory);
+         }
+      });
    })
-   .catch((error) =>
+   .catch((error) => {
+      transactionHistoryDiv.innerHTML = ""
+
+      const noHistoryMessage = document.createElement("div");
+      noHistoryMessage.textContent = "No transaction history available";
+      transactionHistoryDiv.appendChild(noHistoryMessage);
       console.error("Error fetching transaction data:", error)
-   );
+   });
